9474b0f6ff7c20b945a439ae67d44d02b580332e
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-24 01:08:01 -0400

    Update gamelistReviews.php

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -4,7 +4,7 @@
 include 'sqlpassword.php';
 $db = new PDO('mysql:host=localhost;dbname=gamelogapp;', $sqluser, $sqlpassword);
 $sql = "SELECT GameID, GameName, UserID, reviewtext, Date FROM reviews";
-$result = queryDB($sql);
+$result = mysqli_select_db($sql);
 $colCount = 0;
 if (gettype($result) == "object") {
   if ($result->num_rows > 0) {
