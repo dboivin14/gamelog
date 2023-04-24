902636aa2a0201905148e22424a47e9570b1f853
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-23 23:31:34 -0400

    Add files via upload

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -1,4 +1,4 @@
-<?php include 'index.php'; if (isset($_GET['msg']))
+<?php include 'index.php';include("sqlpassword.php");if (isset($_GET['msg']))
   echo "<h2 class='w3-center'>" . $_GET['msg'] . "</h2>";
 
 require 'DBConnect.php';
