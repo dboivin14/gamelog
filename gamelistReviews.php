b83f7112cda55f3f63d990589b8495d0208878fd
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-24 02:20:58 -0400

    s

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -1,4 +1,4 @@
-+/<?php include 'index.php'; if (isset($_GET['msg']))
+<?php include 'index.php'; if (isset($_GET['msg']))
   echo "<h2 class='w3-center'>" . $_GET['msg'] . "</h2>";
 
 include 'sqlpassword.php';
@@ -37,7 +37,7 @@ if (gettype($result) == "object") {
     echo '</div>';
   }
 } else {
-  $data = implode($result);
+  $data = implode(" ", $result);
   echo $data;
   
 }
