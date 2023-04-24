ec8f0724a01263312ec74d2f55388615aecc8bb5
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-24 00:04:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include("sqlpassword.php");
-$db = new PDO('mysql:host=localhost;dbname=gamelogapp;charset=utf8', $username, $password);?>
+$dbname = new PDO('mysql:host=localhost;dbname=gamelogapp;charset=utf8', $username, $password);?>
 <!DOCTYPE html>
 <html>
   <head>
