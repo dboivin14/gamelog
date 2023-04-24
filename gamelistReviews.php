81e7fbb073854828eff2dd67355b164cab5b9918
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-24 03:18:10 -0400

    Update gamelistReviews.php

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -3,10 +3,12 @@
 
 include 'sqlpassword.php';
 $db = new PDO('mysql:host=localhost; dbname=gamelogapp;charset=utf8', $sqluser, $sqlpassword );
-$sql = SELECT GameID, rating, UserID, reviewtext, Date FROM reviews
-    UNION
-  SELECT GameID, GameName FROM games
-  ORDER BY GameID;
+$sql = SELECT GameID, rating, UserID, reviewtext, Date 
+        FROM reviews
+        UNION
+        SELECT GameID, GameName 
+        FROM games
+        ORDER BY GameID;
 $result= $db->query($sql)->fetch();
 $colCount = 0;
 if (gettype($result) == "object") {
