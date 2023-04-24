dca668c68c5fd186901c36307dc0608c4e46cb6c
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-24 00:18:24 -0400

    Update DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -12,7 +12,7 @@ function openDB() {
   global $servername, $username, $password, $dbname, $conn;
 
 // Create connection
-  $conn = new mysqli($servername, $sqluser, $sqlpassword, $dbname);
+  $conn = new mysqli($servername, $username, $password, $dbname);
   if ($conn->connect_error)
     return $conn->connect_error;
   else
