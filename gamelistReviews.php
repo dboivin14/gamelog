0998fc188889536abae05a4985e292142d8c1cfa
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-30 12:12:04 -0400

    Add files via upload

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -6,9 +6,10 @@ $db = new PDO('mysql:host=localhost; dbname=gamelogapp;charset=utf8', $sqluser, 
   //$sql = ('SELECT GameID, rating, UserID, reviewtext, Date FROM reviews ORDER BY GameID');
   //UNION ('SELECT GameID, GameName FROM games ORDER BY GameID');
 $sql = ('(SELECT GameID, rating, UserID, reviewtext, Date FROM reviews ORDER BY gameID DESC)'
-        . 'LEFT JOIN'
-        . '(SELECT GameID, GameName FROM games where GameID = :gameID ORDER BY gameID DESC)'
-        . 'USING (GameID)');
+        //. 'LEFT JOIN'
+        //. '(SELECT GameID, GameName FROM games where GameID = :gameID ORDER BY gameID DESC)'
+        //. 'USING (GameID)'
+        );
   
   $result= $db->prepare($sql);
   $result-> execute();
