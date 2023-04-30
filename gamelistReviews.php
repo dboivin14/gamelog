a5b717252cc32d614e8f6e5ef7fd237043e1b7eb
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-30 11:38:03 -0400

    Add files via upload

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -5,13 +5,13 @@ include 'sqlpassword.php';
 $db = new PDO('mysql:host=localhost; dbname=gamelogapp;charset=utf8', $sqluser, $sqlpassword );
   //$sql = ('SELECT GameID, rating, UserID, reviewtext, Date FROM reviews ORDER BY GameID');
   //UNION ('SELECT GameID, GameName FROM games ORDER BY GameID');
-$sql = $db -> prepare(' 
+$result = $db -> prepare(' 
    (SELECT GameID, rating, UserID, reviewtext, Date FROM reviews ORDER BY gameID DESC)
    LEFT JOIN
    (SELECT GameID, GameName FROM games where GameID = :gameID ORDER BY gameID DESC)
    USING (GameID)');
-  $sql->bindParam(':gameID',$col_start);
-  $result= $db->query($sql);
+  
+  //$result= $db->query($sql);
   $result-> execute();
 $colCount = 0;
 if (gettype($result) == "object") {
