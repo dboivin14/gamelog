c2bf18ed08939a632711dc53e320992b1ba4870c
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-30 16:41:05 -0400

    Add files via upload

--- gamelist.php
+++ gamelist.php
@@ -57,7 +57,7 @@ $result = $db->prepare($sql);
 $result->execute();
 $current = $result->fetchAll(PDO::FETCH_ASSOC);
 
-while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+while ($row = $result->fetchAll(PDO::FETCH_ASSOC)){
   $GameName = $row['GameName'];
 
     ?>
