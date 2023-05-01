6b7d77dd822a1579adab16251381964eb8a2e07f
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-01 13:29:23 -0400

    Update registrationAction.php

--- registrationAction.php
+++ registrationAction.php
@@ -13,18 +13,10 @@
 //include("sqlpassword.php"); //Note from Spencer, needs to use this file here
 //$db = new PDO('mysql:host=localhost;dbname=enter database name here;charset=utf8', $id, $password);
 //$stmt = mysqli_stmt_init($conn);
-//$sql = "INSERT INTO users (username, email, firstname, lastname, password, dob)
-		//VALUES (?,?,?,?,?,?)";
-$data = [
-    'username' => $username,
-    'email' => $email,
-    'firstname' => $firstname,
-    'lastname' => $lastname,
-    'password' => $password,
-    'dob' => $dob
-];
-$sql = "INSERT INTO users (0,0, username, email, firstname, lastname, password, dob) VALUES (:username, :email, :firstname, :lastname, :password, :dob)";
-$stmt= $db->prepare($sql);
+$sql = "INSERT INTO users (username, email, firstname, lastname, password, dob)
+		VALUES (0,0?,?,?,?,?,?)";
+
+$stmt= $pdo->prepare($sql);
 $stmt->execute($data);
 
 //$db->prepare($sql)->execute([$username, $email, $firstname, $lastname, $password, $dob]);
