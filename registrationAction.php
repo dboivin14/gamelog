3076fc502b85026258dd615f1c41fa5598b3f9fe
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-01 12:44:49 -0400

    Update registrationAction.php

--- registrationAction.php
+++ registrationAction.php
@@ -17,7 +17,7 @@ $sql = "INSERT INTO users (username, email, firstname, lastname, password, dob)
 		VALUES (?,?,?,?,?,?)";
 //$stmt = mysqli_stmt_init($conn);
 
-$stmt= $db->prepare($sql);
+$stmt= $pdo->prepare($sql);
 
 $stmt->execute([$username, $email, $firstname, $lastname, $password, $dob]);
 
