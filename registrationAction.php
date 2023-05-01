d01110e3e19936c27ea1531564bec9e064a8e96d
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-01 11:30:29 -0400

    Update registrationAction.php

--- registrationAction.php
+++ registrationAction.php
@@ -10,7 +10,6 @@ $password = $_POST ["password"];
 var_dump($firstname, $lastname, $dob, $email, $username, $password);
 
 
-require __DIR__ . "DBCONNECT.php";
 //include("sqlpassword.php"); //Note from Spencer, needs to use this file here
 //$db = new PDO('mysql:host=localhost;dbname=enter database name here;charset=utf8', $id, $password);
 
