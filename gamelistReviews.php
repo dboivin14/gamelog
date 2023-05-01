f26d48e1d2ed06a7a769662b9dcda75cc933ef1e
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-04-30 21:49:50 -0400

    Add files via upload

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -1,50 +1,50 @@
 <?php include 'index.php';
 
 $sql = "SELECT * reviews LEFT JOIN games ON games.GameID = reviews.GameID";
-            $result= $db->prepare($sql);
-            $result->execute();
-            $rows = $result->fetchAll();
-            foreach ($rows as $games){
-    //while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-echo $row[gamename];
-?>
-<div class="container">
-  <div class="row">
-    <div class="card-group">    
-    <div class="card">
-      <div class="card-header">
-          <h4 class="card-title">GAMEID</h4>
-          <p class="card-text">
-            Game Name: Name<br>
-            Rating: Rating<br>
-            User: ID#<br>
-            Review: ReviewText<br>
-            Date Created: 5/1/23
-          </p>
-        </div>
-    </div>
-    <div class="card">  
-      <?php
-        print_r("Test");
-      ?>
-        <div class="card-body">
-          <h4 class="card-title"><?php echo $row['GameID'] ?></h4>
-          <p class="card-text">
-            Game Name: <?php echo $row['gamename'] ?><br>
-            Rating: <?php echo $row['rating'] ?><br>
-            User: <?php echo $row['ReviewID'] ?><br>
-            Review: <?php echo $row['reviewtext'] ?><br>
-            Date Created: <?php echo $row['Date'] ?>
-          </p>
-        </div>
-      
-      
+$result= $db->prepare($sql);
+$result->execute();
+//$rows = $result->fetchAll();
+//foreach ($rows as $games){
+if($result->num_rows>0){
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  //echo $row[gamename];
+    ?>
+  }
+    <div class='card-group'>
+      <div class='card'>
+        <div class="card-header">
+            <h4 class="card-title">GAMEID</h4>
+            <p class="card-text">
+              Game Name: Name<br>
+              Rating: Rating<br>
+              User: ID#<br>
+              Review: ReviewText<br>
+              Date Created: 5/1/23
+            </p>
+          </div>
       </div>
-    </div>
-  </div>
-</div>
-<?php 
-      }
+      <div class="card">  
+        <?php
+          print_r("Test");
+        ?>
+          <div class="card-body">
+            <h4 class="card-title"><?php echo $row['GameID'] ?></h4>
+            <p class="card-text">
+              Game Name: <?php echo $row['gamename'] ?><br>
+              Rating: <?php echo $row['rating'] ?><br>
+              User: <?php echo $row['ReviewID'] ?><br>
+              Review: <?php echo $row['reviewtext'] ?><br>
+              Date Created: <?php echo $row['Date'] ?>
+            </p>
+          </div>
+
+
+        </div>
+      </div>
+  <?php 
+  ';
+  }
+}
  ?>  
 
 
