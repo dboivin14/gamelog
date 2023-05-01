029c9a45db69797c7dc7f690e91fac3267dee70b
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-01 00:35:06 -0400

    Add files via upload

--- gamelist.php
+++ gamelist.php
@@ -43,6 +43,9 @@
 </head>
 <body>
 
+
+  
+  
 <h2>All Games</h2>
   <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for games.." title="Type in a game">
 
@@ -70,9 +73,36 @@ $colCount =+ 1;
     <tr>
       <td><?php echo $row['gamename']."<br />\n"?></td>
       <td><?php echo $row['rating']."<br />\n"?></td>
+    <button type="button" class="btn btn-primary" data-bs-toggle='modal' data-bs-target='#myModal'>
+      Rate Game
+    </button>
     </tr>
   
+  <!--Modal-->
+  <div class='modal' id='myModal'>
+    <div class='modal-dialog'>
+      <div class='modal-content'>
+        
+        <!--Modal Header-->
+        <div class="modal-header">
+        <h4 class="modal-title"><?php echo $row['gamename']?></h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
 
+        <!-- Modal body -->
+        <div class="modal-body">
+          The game's current rating: <?php echo $row['rating']?> / 5
+          Enter your own rating: X
+        </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
+      
+      </div>
+    </div>
+  </div>
   
 <?php
 }
