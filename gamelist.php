4ac2b020e6650afc773fce6998a88f254098c7bf
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-01 01:11:12 -0400

    Add files via upload

--- gamelist.php
+++ gamelist.php
@@ -103,8 +103,7 @@ $colCount =+ 1;
   </div>
   
           <script src="~/scripts/jquery.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
         <script type="text/javascript">
             $(function changerating() {
