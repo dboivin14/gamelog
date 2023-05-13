69715b0d18240a2af090b80294946caaca57183c
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-13 11:29:58 -0400

    Update gamelistReviews.php

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -68,7 +68,7 @@ if($result->rowCount()>0){
               Review: <?php echo $row['reviewtext'] ?><br>
               Date Created: <?php echo $row['Date'] ?>
             </p>
-              <button type="button" class="btn btn-primary" data-bs-toggle='modal' **data-bs-target='#myModal<?php echo $row['GameID']?>'**>Rate Game</button></td>
+              <button type="button" class="btn btn-primary" data-bs-toggle='modal' data-bs-target='#myModal<?php echo $row['GameID']?>'>Rate Game</button></td>
           </div>
       </div>
 
@@ -79,14 +79,14 @@ if($result->rowCount()>0){
 ?>
         <!--Modal-->
        
-  <div class='modal' **id='myModal<?php echo $row['GameID']?>'** tabindex="-1" role="dialog" data-backdrop="false" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class='modal' id='myModal<?php echo $row['GameID']?>' tabindex="-1" role="dialog" data-backdrop="false" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class='modal-dialog'>
       <div class='modal-content'>
         
         <!--Modal Header-->
         <div class="modal-header">
         <h4 class="modal-title" id="exampleModalLabel">Review the Game!</h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal">&times;</button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
 
         <!-- Modal body -->
