e67f51bae3a8c51b79b1fa5e1440e089a7b4a326
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-13 08:45:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -39,8 +39,8 @@ $db = new PDO('mysql:host=localhost;dbname=gamelogapp;', $sqluser, $sqlpassword)
     <title>Game Log Test Page</title>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
   </head>
 <body>
   <ul class="topnav">
