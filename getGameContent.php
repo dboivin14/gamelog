39fdcf5642060a66f2b5f608486a93a53ed0cf9e
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-13 07:33:28 -0400

    Update getGameContent.php

--- getGameContent.php
+++ getGameContent.php
@@ -1,6 +1,8 @@
 <?php 
   //Open db and call query
-
+    $sql = "SELECT * FROM reviews LEFT JOIN games ON games.GameID = reviews.GameID";
+    $result= $db->prepare($sql);
+    $result->execute();
   //Initiate 
      
     if($query->num_rows > 0){ 
