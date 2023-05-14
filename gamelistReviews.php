e9dc3c98f211eaf83d235086527ab79c055bca08
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-14 13:22:27 -0400

    Update gamelistReviews.php

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -50,7 +50,7 @@ if($result->rowCount()>0){
 
 <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for games.." title="Type in a game"> 
     <div class='row' id='row'>
-      <div class ='col-large'>
+      <div class ='col-lg'>
       <div class='card'>
         <div class="card-header">
             <h4 class="card-title">GAMES:</h4>
