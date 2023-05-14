e582f779450617e8e1332d44ba22bc300f3fe95f
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-14 17:05:26 -0400

    Update gamelistReviews.php

--- gamelistReviews.php
+++ gamelistReviews.php
@@ -54,7 +54,7 @@ if($result->rowCount()>0){
       <?php
       while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
       ?>
-      <div class ='col'>
+      <div class ='col' span='4'>
       <div class="card" id='card'>  
           <div class="card-body">
             <h4 class="card-title"><?php echo $row['gamename'] ?></h4>
