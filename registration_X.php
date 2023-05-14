355a37e669c7b7b8708c4f2e2b629374d0b1b221
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-14 11:29:13 -0400

    Update registration_X.php

--- registration_X.php
+++ registration_X.php
@@ -5,37 +5,37 @@
   <form name="registration" action="registrationAction.php" class="was-validated">
       <div class="mb-3">
         <label for="firstname" class="form-label">First Name:</label>
-        <input type="text" class="form-control" id="firstname" placeholder="Enter first name" name="firstname" required>
+        <input type="text" class="form-control" id="firstname" placeholder="Enter first name" name="firstname" autocomplete="off" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
       </div>
       <div class="mb-3">
         <label for="lastname" class="form-label">Last name:</label>
-        <input type="text" class="form-control" id="lastname" placeholder="Enter your lastname" name="lastname" required>
+        <input type="text" class="form-control" id="lastname" placeholder="Enter your lastname" name="lastname" autocomplete="off" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
       </div>
       <div class="mb-3">
         <label for="dob" class="form-label">Date of Birth:</label>
-        <input type="text" class="form-control" id="dob" placeholder="dd/mm/yyyy" name="dob" required>
+        <input type="text" class="form-control" id="dob" placeholder="dd/mm/yyyy" name="dob" autocomplete="off" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email:</label>
-        <input type="text" class="form-control" id="email" placeholder="user@example.com" name="email" required>
+        <input type="text" class="form-control" id="email" placeholder="user@example.com" name="email" autocomplete="off" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
       </div>
       <div class="mb-3 mt-3">
         <label for="user" class="form-label">Username:</label>
-        <input type="text" class="form-control" id="username" placeholder="Enter username" name="username" required>
+        <input type="text" class="form-control" id="username" placeholder="Enter username" name="username" autocomplete="off" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">Password:</label>
-        <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" required>
+        <input type="password" class="form-control" id="password" placeholder="Enter password" name="password" autocomplete="off" required>
         <div class="valid-feedback">Valid.</div>
         <div class="invalid-feedback">Please fill out this field.</div>
       </div>      
