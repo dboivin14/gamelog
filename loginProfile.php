a85f5f97f6a3698df6ac21b956b92336985f29e5
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-15 00:05:36 -0400

    Update loginProfile.php

--- loginProfile.php
+++ loginProfile.php
@@ -4,3 +4,17 @@ session_start();
 print_r($_SESSION);
 
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Profile </title>
+    <meta charset= "UTF-8">
+    
+  </head>
+  <body>
+    
+    <h1> Personal Page </h1>
+    
+  </body>
+</html>
