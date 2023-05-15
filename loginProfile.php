521b70e5dc46a1d451165dbb74617fa46bc4d24a
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-15 00:54:31 -0400

    Update loginProfile.php

--- loginProfile.php
+++ loginProfile.php
@@ -41,6 +41,19 @@ $db = new PDO('mysql:host=localhost;dbname=gamelogapp;', $sqluser, $sqlpassword)
         ul.topnav li.right, 
         ul.topnav li {float: none;}
       }
+   button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
        .cancelbtn {
           width: auto;
           padding: 10px 18px;
