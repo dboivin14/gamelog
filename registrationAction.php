b2f95ff93e4a21acc52df7cba4a9cd39ea075621
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-14 22:53:24 -0400

    Update registrationAction.php

--- registrationAction.php
+++ registrationAction.php
@@ -8,26 +8,14 @@ $password = $_GET["password"];
 $dob = $_GET["dob"];
 
 
-//include("sqlpassword.php"); //Note from Spencer, needs to use this file here
-//$db = new PDO('mysql:host=localhost;dbname=enter database name here;charset=utf8', $id, $password);
-//$stmt = mysqli_stmt_init($conn);
+
 $sql = "INSERT IGNORE INTO users (username, email, firstname, lastname, password, dob)
 		VALUES (?,?,?,?,?,?)";
 
 $stmt= $db->prepare($sql);
 $stmt->execute([$username, $email, $firstname, $lastname, $password, $dob]);
 
-//$db->prepare($sql)->execute([$username, $email, $firstname, $lastname, $password, $dob]);
 
-//mysqli_stmt_bind_param($stmt, "sssss",
-//					   $username,
-//					   $email,
-//					   $firstname,
-//					   $lastname,
-//					   $password,
-//					   $dob);
-
-//mysqli_stmt_execute($stmt);
 
 
 
