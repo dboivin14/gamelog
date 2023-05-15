e322ad0a81cadc61266f07aa69b65b3eebe7473e
SpenxG <38442848+SpenxG@users.noreply.github.com>
2023-05-15 11:26:01 -0400

    Update gamelistGameAdd.php

--- gamelistGameAdd.php
+++ gamelistGameAdd.php
@@ -2,14 +2,17 @@
 
 
   
-  $GameID = (int)$_GET['GameID'];
-  $stmt = $pdo -> prepare('SELECT * FROM games WHERE GameID = ?');
-  $stmt -> execute([$_GET['GameID']]);
+  $GameID = $_GET['GameID'];
+  //$stmt = $pdo -> prepare('SELECT * FROM games WHERE GameID = ?');
+  //$stmt -> execute([$_GET['GameID']]);
   
-  $game = $stmt->fetch(PDO::FETCH_ASSOC);
+ // $game = $stmt->fetch(PDO::FETCH_ASSOC);
   
   $sql = "INSERT IGNORE INTO usergames (GameID, Date)
-		VALUES (?,?)";                
+		VALUES (?,?)";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
   $_SESSION['usergames'] = array($gameID)
 	  
   
