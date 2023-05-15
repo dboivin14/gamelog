9fa13646982d560fa148214f6eef7874e145dab5
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-15 00:20:37 -0400

    Update loginProfile.php

--- loginProfile.php
+++ loginProfile.php
@@ -4,55 +4,7 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-      body {
-        margin: 0;
-        background-color: grey;
-      }
-      
-      ul.topnav {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #333;
-      }
-
-      ul.topnav li {float: left;}
-
-      ul.topnav li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-      }
-
-      ul.topnav li a:hover:not(.active) {background-color: #111;}
-
-      ul.topnav li a.active {background-color: #04AA6D;}
-
-      ul.topnav li.right {float: right;}
-  @media screen and (max-width: 600px) {
-        ul.topnav li.right, 
-        ul.topnav li {float: none;}
-      }
-      </style>
-
-  </head>
-<body>
-  <ul class="topnav">
-  <li><a class="active" href="index.php">Home</a></li>
-  <li><a href="gamelist.php">Games</a></li>
-  <li><a href="gamelistReviews.php">Reviews</a></li>
  
-    <li class="right"><a href="registration_X.php">Register</a></li>
-    <li class="right"><a href="login_X.php">Login</a></li>
- 
-  
-</ul>
   
   <h1>Personal Page</h1>
 </html>
