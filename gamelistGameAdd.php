e78139955fb612b0f0c6165ea642fec84389f483
TyssenLaBombard <95341145+TyssenLaBombard@users.noreply.github.com>
2023-05-15 10:58:15 -0400

    Update gamelistGameAdd.php

--- gamelistGameAdd.php
+++ gamelistGameAdd.php
@@ -10,16 +10,15 @@
   
   $sql = "INSERT IGNORE INTO usergames (UserGamesID, UserID, GameID, Date)
 		VALUES (?,?,?,?)";                
-                   
+  $_SESSION['usergames'] = array($gameID)
+	  
   if(isset($_SESSION['usergames']) && is_array($_SESSION['usergames'])) {
       if (array_key_exists($GameID, $_SESSION['usergames'])) {
           echo 'game already in list';
       } else {
       $_SESSION['usergames'][$GameID] = 1
 	      }
- else {
-   $_SESSION['usergames'] = array($gameID)
- }
+ 
   
   
 ?>
